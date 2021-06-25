20c815751407d90729ccd396e158634851018fa8
Jon-Michael Narvaez <you@example.com>
2021-06-25 15:47:02 -0500

    more changes

--- client/src/pages/errorpage.js
+++ client/src/pages/errorpage.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { motion } from "framer-motion";
 
 const FourOhFour = () => {
   return (
-    <div>
-      <p>Oh snap, this pages isn't real!</p>
-    </div>
+    <motion.div className="container">
+      <Row>
+        <Col>
+          <p>Oh snap, this pages isn't real!</p>
+        </Col>
+      </Row>
+    </motion.div>
   );
 };
 
